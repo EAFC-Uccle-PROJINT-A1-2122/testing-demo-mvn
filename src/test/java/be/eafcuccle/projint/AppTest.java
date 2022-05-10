6668e93833366b4850fbfc92972a047cbd3aabf8
Francois ROLAND <francois.roland@iepscf-uccle.be>
2022-05-10 05:56:48 +0000

    Create JUnit maven project structure.

---
+++ src/test/java/be/eafcuccle/projint/AppTest.java
@@ -0,0 +1,12 @@
+package be.eafcuccle.projint;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
